fe07b05b1414ffe5fa9a9fb9876ff2d1c414d2b5
DongChen <2634403005@qq.com>
2023-06-29 11:41:46 +0800

    feat: 添加Git提交规范

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,7 +15,7 @@ module.exports = {
       jsx: true
     }
   },
-  extends: ['plugin:vue/vue3-recommended', 'plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
+  extends: ['eslint:recommended', 'plugin:vue/vue3-recommended', 'plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
   globals: {
     defineProps: 'readonly',
     defineEmits: 'readonly',
@@ -23,36 +23,32 @@ module.exports = {
     withDefaults: 'readonly'
   },
   rules: {
-    'vue/script-setup-uses-vars': 'error',
+    'no-unused-vars': 'off',
+    'no-case-declarations': 'off',
+    'no-use-before-define': 'off',
+    'space-before-function-paren': 'off',
+
     '@typescript-eslint/ban-ts-ignore': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
-    '@typescript-eslint/no-var-requires': 'always',
+    '@typescript-eslint/no-var-requires': 'off',
     '@typescript-eslint/no-empty-function': 'off',
-    'vue/custom-event-name-casing': 'off',
-    'no-use-before-define': 'off',
     '@typescript-eslint/no-use-before-define': 'off',
     '@typescript-eslint/ban-ts-comment': 'off',
     '@typescript-eslint/ban-types': 'off',
     '@typescript-eslint/no-non-null-assertion': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-unused-vars': [
-      'always',
+      'error',
       {
         argsIgnorePattern: '^_',
         varsIgnorePattern: '^_'
       }
     ],
-    'no-unused-vars': [
-      'always',
-      {
-        argsIgnorePattern: '^_',
-        varsIgnorePattern: '^_'
-      }
-    ],
-    'color-function-notation': 'legacy',
-    'space-before-function-paren': 'off',
+
+    'vue/script-setup-uses-vars': 'error',
     'vue/attributes-order': 'off',
+    'vue/custom-event-name-casing': 'off',
     'vue/one-component-per-file': 'off',
     'vue/html-closing-bracket-newline': 'off',
     'vue/max-attributes-per-line': 'off',
